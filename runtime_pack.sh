74d8f007878d2ad916623606555e999e24e16d20
Beniamin Barret <b.barret@voipzilla.ca>
2021-12-14 18:41:55 +0100

    Update dockerfile, add runtime_pach.sh

---
+++ runtime_pack.sh
@@ -0,0 +1,40 @@
+if [ $# -ge 2 ];then
+    if [ $2 == "build" ];then
+        ./build.sh
+    fi
+  fi
+
+if [ $# -ge 3 ];then
+    if [ $3 == "build" ];then
+        ./build.sh
+    fi
+  fi
+
+if [ $# -ge 2 ];then
+    if [ $2 == "install" ];then
+        ./install.sh
+    fi
+  fi
+
+if [ $# -ge 3 ];then
+    if [ $3 == "install" ];then
+        ./install.sh
+    fi
+  fi
+
+
+# COPY
+
+# Server
+docker cp ./server/ $1:${APP_PATH}/
+
+# Scripts
+docker cp ./scripts/ $1:${APP_PATH}/public/scripts/
+
+# html
+docker cp ./index.html $1:${APP_PATH}/public/
+
+# Qt Webassembly
+docker cp ./Innox.wasm $1:${APP_PATH}/public/
+docker cp ./qtloader.js $1:${APP_PATH}/public/
+docker cp ./Innox.js $1:${APP_PATH}/public/
